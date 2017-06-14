04784def05fab42460c338290f704e16860920dc
Marcos Paliari <marcos@paliari.com.br>
2017-06-14 09:49:15 -0300

    implement ModelException.getValidatorErrors

--- src/ModelException.php
+++ src/ModelException.php
@@ -1,6 +1,33 @@
 <?php
+
 namespace Paliari\Doctrine;
 
-class ModelException extends \Exception
+use Exception;
+
+class ModelException extends Exception
 {
+
+    protected $validator_errors;
+
+    /**
+     * ValidatorErrorsException constructor.
+     *
+     * @param string|ValidatorErrors $message
+     * @param int                    $code
+     * @param Exception|null         $previous
+     */
+    public function __construct($message = "", $code = 0, Exception $previous = null)
+    {
+        $this->validator_errors = $message;
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @return ValidatorErrors
+     */
+    public function getValidatorErrors()
+    {
+        return $this->validator_errors;
+    }
+
 }
